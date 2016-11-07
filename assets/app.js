5e3466ddfeb56717c7fd2f252716f6549ed625f2
Alan <a-ashbeck@northwestern.edu>
2016-11-07 12:29:56 -0600

    Finalize logic and update index.html

---
+++ assets/app.js
@@ -0,0 +1,60 @@
+var topics = [
+    'Skwisgaar Skwigelf', 'Guitars', 'Abbath', 'Bunnies',
+    'GOBLINS', 'Exactly 100 beers', 'SO MANY CHICKENS',
+    'All of the unicorns in the galazy', 'YOU DON\'T KNOW ME!',
+    'This is the end.'
+];
+
+function generateInitialButtons(){
+    $('#buttons').empty();
+
+    for (var i = 0; i < topics.length; i++){
+        var button = $('<button>');
+        button.addClass('gif');
+        button.attr('data-topic', topics[i]);
+        button.text(topics[i]);
+        $('#buttons').append(button);
+    }
+}
+
+$(document).ready(function() {
+    $(document).on('click', '.gif', function() {
+        var searchTopic = $(this).data('topic');
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $('#gifs').empty();
+
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+        }).done(function(response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $('<div class="item">')
+
+                var rating = results[i].rating;
+
+                var paragraph = $('<p>').text("Rating: " + rating);
+
+                var image = $('<img>');
+                image.attr('src', results[i].images.fixed_height.url);
+
+                gifDiv.append(paragraph);
+                gifDiv.append(image);
+
+                $('#gifs').prepend(gifDiv);
+            }
+        });
+    });
+
+    $('#add-gif').on('click', function(){
+        var gifSearch = $('#gif-search').val().trim();
+
+        topics.push(gifSearch);
+        generateInitialButtons();
+        return false;
+    });
+
+    generateInitialButtons();
+});
